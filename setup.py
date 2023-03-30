8b0551b559f583c471c165ef8b6c0a6654108183
Chaska-de-ideas <128561934+Chaska-de-ideas@users.noreply.github.com>
2023-03-30 16:17:31 -0300

    Actualización v 0.2.2

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name='Goog_API',
     description="Librería Python con métodos para las APIs de Google. Docs, Sheets, Drive, Gmail y Calendar. Desarrollada por Hernán A. Teszkiewicz Novick, para el equipo dev. de Ch'aska.",
-    version='0.21',
+    version='0.22',
     author           = 'Hernán A. Teszkiewicz Novick',
     author_email     = 'herni@cajadeideas.ar',
     license          =  'MIT'    ,
     url= 'https://github.com/Chaska-de-ideas/Goog_API',
-    download_url     =  'https://github.com/Chaska-de-ideas/Goog_API/raw/main/dist/Goog_API-0.21.tar.gz',
-    packages=find_packages(),
+    download_url     =  'https://github.com/Chaska-de-ideas/Goog_API/raw/main/dist/Goog_API-0.22.tar.gz',
+    packages=['Goog_API'],
     install_requires=[
         'google-api-python-client',
         'google-auth',
